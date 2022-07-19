3b2d5eb0b96f7d9e61a63c6c807358cd70badf36
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-07-19 16:04:07 +0200

    Last commit

--- src/main/java/fr/eni/Spring04demoData/dal/ContactDao.java
+++ src/main/java/fr/eni/Spring04demoData/dal/ContactDao.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import fr.eni.Spring04demoData.bo.Contact;
 
-public interface ContactDao extends CrudRepository<Contact, Integer>{
+public interface ContactDao extends CrudRepository<Contact, Integer> {
 
 }
